cdc855dc8738cc9c70e24a184efa90f714e82610
Nicholas Robinson <me@nicholassavilerobinson.com>
2012-12-23 21:39:56 -0800

    Initial Commit

---
+++ BuddyBox/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  BuddyBox
+//
+//  Created by Nicholas Robinson on 12/23/12.
+//  Copyright (c) 2012 Nicholas Robinson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
